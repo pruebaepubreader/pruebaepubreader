127a2f9d26e14be48fc53ed4121e786fff736b16
pruebaepubreader <prueba.epub.reader@gmail.com>
2013-09-06 03:09:26 +0200

    List view not yet ordered by name or date

--- PruebaEpubReader/src/com/prueba/pruebaepubreader/MainActivity.java
+++ PruebaEpubReader/src/com/prueba/pruebaepubreader/MainActivity.java
@@ -20,23 +20,34 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener  {
 
 	private static final String appKey = "6kqi2xfu21urmde";
 	private static final String appSecret = "etd89oey5ebpiwz";
 	private static final int REQUEST_LINK_TO_DBX = 0;
-	private TextView output;
-
+	
+	private ViewHolder [] viewHolders;
+	
+	private Spinner spinner;
+	private ListView bookList;
 	private Button mLinkButton;
 	private DbxAccountManager mDbxAcctMgr;
-	DbxFileSystem dbxFs;
-	List<DbxFileInfo> infos;
+	private DbxFileSystem dbxFs;
+	private List<DbxFileInfo> infos;
 	
 	private MainActivityHandler handler = new MainActivityHandler(this);
 	private ProgressDialog dialogo;
@@ -47,15 +58,21 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		infos = new LinkedList<DbxFileInfo>();
-		output = (TextView) findViewById(R.id.output);
 		mLinkButton = (Button) findViewById(R.id.link_button);
+		bookList = (ListView) findViewById(R.id.bookList);
 		mLinkButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				onClickLinkToDropbox();
 			}
 		});
-		mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret);
+		mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), appKey, appSecret); //TODO
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.spinner_array, android.R.layout.simple_spinner_item);
+		spinner = (Spinner) findViewById(R.id.spinner);
+		spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 	}
 
 	@Override
@@ -70,7 +87,7 @@ public class MainActivity extends Activity {
 		super.onResume();
 		if (mDbxAcctMgr.hasLinkedAccount()) {
 			showLinkedView();
-			doDropboxTest();
+			getEpubFiles();
 		} else {
 			showUnlinkedView();
 		}
@@ -82,13 +99,14 @@ public class MainActivity extends Activity {
 
 	private void showLinkedView() {
 		mLinkButton.setVisibility(View.GONE);
-		output.setVisibility(View.VISIBLE);
-		
+		bookList.setVisibility(View.VISIBLE);
+		spinner.setVisibility(View.VISIBLE);
 	}
 
 	private void showUnlinkedView() {
 		mLinkButton.setVisibility(View.VISIBLE);
-		output.setVisibility(View.GONE);
+		bookList.setVisibility(View.GONE);
+		spinner.setVisibility(View.GONE);
 	}
 	
 	private void recursiveFileSearch(DbxPath path){
@@ -106,7 +124,7 @@ public class MainActivity extends Activity {
 		}
 	}
 	
-	private void doDropboxTest() {
+	private void getEpubFiles() {
         try {
             // Create DbxFileSystem for synchronized file access.
             dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
@@ -118,15 +136,13 @@ public class MainActivity extends Activity {
 			Thread comunicaciones = new Thread(){
 				public void run(){
 					recursiveFileSearch(DbxPath.ROOT);
-		            
-					//Enviamos al servidor los datos del usuario
-					MainActivity.this.handler.sendEmptyMessage(0);
+		            MainActivity.this.handler.sendEmptyMessage(0);
 				}
 			};
 			comunicaciones.start();
             
         } catch (IOException e) {
-        	output.setText("Dropbox test failed: " + e);
+        	e.printStackTrace();
         }
     }
 	
@@ -138,17 +154,60 @@ public class MainActivity extends Activity {
         switch(msg.what) {
 	        case 0:
 	        	dialogo.dismiss();
-	        	printFiles();
+	        	//printFiles();
+	        	listFiles();
 	        break;
         }
     }
 	
-	private void printFiles(){
-		output.setText("\nContents of app folder:\n");
-        for (DbxFileInfo info : infos) {
-        	output.append("    " + info.path + ", " + info.modifiedTime + '\n');
-        }
+	private void listFiles(){
+		viewHolders = new ViewHolder[infos.size()];
+		BookListAdapter bookListAdapter = new BookListAdapter(this);
+		bookList.setAdapter(bookListAdapter);
 	}
+	/*private void printFiles(){
+		for (DbxFileInfo info : infos) {
+			output.append("    " + info.path + ", " + info.modifiedTime + '\n');
+        }
+	}*/
+	
+	private class BookListAdapter extends ArrayAdapter<Object>{
+		Activity context;
+		
+		public BookListAdapter(Activity context){
+			super(context, R.layout.listelement, viewHolders);
+			this.context = context;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent){
+			View item = convertView;
+			ViewHolder holder;
+			
+			if(item ==null){
+				LayoutInflater inflater = context.getLayoutInflater();
+				item = inflater.inflate(R.layout.listelement, null);
+				
+				holder = new ViewHolder();
+				holder.bookicon = (ImageButton) item.findViewById(R.id.bookicon);
+				holder.bookname = (TextView) item.findViewById(R.id.bookname);
+				holder.date = (TextView) item.findViewById(R.id.date);
+				
+				item.setTag(holder);
+			}else{
+				holder = (ViewHolder)item.getTag();
+			}
+			holder.bookname.setText(infos.get(position).path.toString());
+			holder.date.setText(infos.get(position).modifiedTime.toString());
+			return (item);
+		}
+	}
+	
+	private static class ViewHolder{
+		ImageButton bookicon;
+		TextView bookname;
+		TextView date;
+	}
+	
 	
 	/**
 	 * Implementa el handler para las comunicaciones
@@ -173,4 +232,23 @@ public class MainActivity extends Activity {
         	}
         }
     }
+	
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View arg1, int position, long arg3) {
+		// TODO Auto-generated method stub
+		setOrder((String) parent.getItemAtPosition(position));
+	}
+	
+	private void setOrder(String order){
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
